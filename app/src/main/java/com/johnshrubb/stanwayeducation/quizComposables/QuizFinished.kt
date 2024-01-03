4ca42292fbdb1c9bcf3cb71c6d12c076a23359d4
John Shrubb <john-shrubb@users.noreply.github.com>
2024-01-03 17:09:37 +0000

    Fix minor visual bug.

--- app/src/main/java/com/johnshrubb/stanwayeducation/quizComposables/QuizFinished.kt
+++ app/src/main/java/com/johnshrubb/stanwayeducation/quizComposables/QuizFinished.kt
@@ -6,6 +6,9 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.text.style.TextAlign
 import androidx.navigation.NavController
@@ -19,6 +22,7 @@ fun QuizFinished(
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally,
     ) {
+        val quizScore by remember { mutableIntStateOf(quizState.currentScore) }
         // Congratulatory message.
         Text(
             text = "Congratulations! You finished the quiz!",
@@ -27,7 +31,7 @@ fun QuizFinished(
         )
         // Final score message.
         Text(
-            text = "Your final score was:\n${quizState.currentScore}",
+            text = "Your final score was:\n${quizScore}",
             style = MaterialTheme.typography.headlineLarge,
             textAlign = TextAlign.Center,
         )
